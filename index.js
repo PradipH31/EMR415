8eed66cad6cc901f707b9be3015b910c8286c903
pradiph31 <pradip.hamal31@gmail.com>
2022-04-12 19:52:29 -0500

    removed refernces to old data from the index

--- index.js
+++ index.js
@@ -38,24 +38,24 @@ express()
   .use(bodyParser.urlencoded({ extended: true }))
   .use(bodyParser.json())
   .use(bodyParser.raw())
-  .post('/emr', (req, res) => {
-    new_record = {
-      "id": data.length + 1,
-      "name": req.body.name,
-      "email": req.body.email,
-      "gender": req.body.gender,
-      "phone": req.body.phone,
-      "address": req.body.address
-    }
-    if (new_record.name && new_record.email && new_record.gender && new_record.phone && new_record.address) {
-      data.push(new_record)
-      console.log(new_record)
-      res.sendStatus(200)
-    } else
-      res.sendStatus(400)
-  })
-  .get('/emr/:id', (req, res) => {
-    const id = req.params.id;
-    res.send(JSON.stringify(data[id - 1]))
-  })
+  // .post('/emr', (req, res) => {
+  //   new_record = {
+  //     "id": data.length + 1,
+  //     "name": req.body.name,
+  //     "email": req.body.email,
+  //     "gender": req.body.gender,
+  //     "phone": req.body.phone,
+  //     "address": req.body.address
+  //   }
+  //   if (new_record.name && new_record.email && new_record.gender && new_record.phone && new_record.address) {
+  //     data.push(new_record)
+  //     console.log(new_record)
+  //     res.sendStatus(200)
+  //   } else
+  //     res.sendStatus(400)
+  // })
+  // .get('/emr/:id', (req, res) => {
+  //   const id = req.params.id;
+  //   res.send(JSON.stringify(data[id - 1]))
+  // })
   .listen(PORT, () => console.log(`Listening on ${PORT}` + data))
