236bed9170f04df9e675e3512aaf8a52d4a4f541
pradiph31 <pradip.hamal31@gmail.com>
2022-04-12 20:53:19 -0500

    added getbyid

--- index.js
+++ index.js
@@ -3,33 +3,24 @@ const bodyParser = require('body-parser')
 const path = require('path')
 const { MongoClient } = require("mongodb");
 const PORT = process.env.PORT || 5000
+
 const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, { useUnifiedTopology: true });
+const database = client.db('415Phase2EMR');
+const emrs = database.collection('emr');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/emr', async function (req, res) {
-    const client = new MongoClient(uri, { useUnifiedTopology: true });
-
     try {
       await client.connect();
-
-      const database = client.db('415Phase2EMR');
-      const emrs = database.collection('emr');
-
-
-      // await emrs.find({}).toArray(function (err, result) {
-      //   if (err) throw err;
-      //   res.send(JSON.stringify(result));
-      // });
-
       const query = {};
       const cursor = await emrs.find({})
       const result = await cursor.toArray();
       res.send(JSON.stringify(result));
       console.log(result)
-
     } catch (err) {
       console.log(err);
     }
@@ -56,8 +47,18 @@ express()
   //   } else
   //     res.sendStatus(400)
   // })
-  // .get('/emr/:id', (req, res) => {
-  //   const id = req.params.id;
-  //   res.send(JSON.stringify(data[id - 1]))
-  // })
+  .get('/emr/:id', (req, res) => {
+    try {
+      await client.connect();
+      const query = { "id": req.params.id };
+      const result = await emrs.findOne(query);
+      res.send(JSON.stringify(result));
+      // console.log(result)
+    } catch (err) {
+      console.log(err);
+    }
+    finally {
+      await client.close();
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
