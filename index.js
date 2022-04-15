b00edc235bea20636fa366f6d2fc32ad667b6f16
pradiph31 <pradip.hamal31@gmail.com>
2022-04-15 18:40:50 -0500

    fixed updateOne

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
+//parse arguments from request body with body-parser
 const bodyParser = require('body-parser')
+
 const path = require('path')
+//connect to mongodb 
 const { MongoClient } = require("mongodb");
+
 const PORT = process.env.PORT || 5000
 
+//get the uri from environment variables
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, { useUnifiedTopology: true });
 const database = client.db('415Phase2EMR');
@@ -13,10 +18,14 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
+  
+  //get all api
   .get('/emr', async (req, res) => {
     try {
       await client.connect();
+      //empty query to get everything
       const query = {};
+
       const cursor = await emrs.find({})
       const result = await cursor.toArray();
       res.send(JSON.stringify(result));
@@ -28,6 +37,8 @@ express()
       await client.close();
     }
   })
+
+  //get by id api
   .get('/emr/:id', async (req, res) => {
     try {
       await client.connect();
@@ -41,25 +52,38 @@ express()
       await client.close();
     }
   })
+
+  //use bodyparser to parse arguments
   .use(bodyParser.urlencoded({ extended: true }))
   .use(bodyParser.json())
   .use(bodyParser.raw())
+  
+  //post api
   .post('/emr', async (req, res) => {
     try {
       await client.connect();
+
+      //get the old id and add 1, then convert that id to string
       old_id = await emrs.findOne({}, { $natural: -1 })
       new_id = (Number(old_id.id) + 1).toString(10)
+
+      //create the new record with the updated id and information from request body
       new_record = {
         "id": new_id,
         "name": req.body.name,
         "dob": req.body.dob,
         "medications": req.body.medications
       }
+
+      //check if fields are empty
       if (new_record.name && new_record.dob && new_record.medications) {
         await emrs.insertOne(new_record)
         res.sendStatus(200)
+
       } else
+        // if empty fields, send bad request
         res.sendStatus(400)
+
     } catch (err) {
       console.log(err);
     }
@@ -67,15 +91,22 @@ express()
       await client.close();
     }
   })
+
+  //delete api
   .delete('/emr/:id', async (req, res) => {
     try {
       await client.connect();
       const query = { "id": req.params.id };
+
+      //check if the emr actually exists
       const emr = await emrs.findOne(query);
       if (emr) {
+        //if emr exists, delete it and send OK
         const result = await emrs.deleteOne(query);
         res.send(result.acknowledged);
       }
+
+      //if emr doesn't exist, send false for not deleted
       res.send(false);
     } catch (err) {
       console.log(err);
@@ -84,10 +115,14 @@ express()
       await client.close();
     }
   })
+
+  //put update
   .put('/emr/:id', async (req, res) => {
     try {
       await client.connect();
       const query = { "id": req.params.id };
+
+      //get the record
       const emr = await emrs.findOne(query);
       new_record = {
         "name": req.body.name,
@@ -95,10 +130,16 @@ express()
         "id": req.params.id,
         "medications": req.body.medications
       }
+
+      //check if the fields are empty or if the emr with the id exists
       if (new_record.name && new_record.dob && new_record.medications && emr) {
+
+        //everything is fine, so update the record
         await emrs.updateOne(query, { '$set': new_record }, { upsert: false })
         res.sendStatus(200)
       } else
+
+        //something has gone wrong, so send bad request
         res.sendStatus(400)
     } catch (err) {
       console.log(err);
